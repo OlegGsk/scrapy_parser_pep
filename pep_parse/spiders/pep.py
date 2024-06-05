babfe49db7ddfeeec34b1c5865b40b56af535f8d
Олег Говоровский <oleq.gov@ya.ru>
2024-06-05 11:14:19 +0500

    add spider

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,10 +1,24 @@
 import scrapy
+from pep_parse.items import PepParseItem
+import re
 
 
 class PepSpider(scrapy.Spider):
     name = 'pep'
     allowed_domains = ['peps.python.org']
-    start_urls = ['http://peps.python.org/']
+    start_urls = ['https://peps.python.org/']
 
     def parse(self, response):
-        pass
+        all_links_peps = response.css(
+            'section#numerical-index a::attr(href)').getall()
+        for link in all_links_peps:
+            yield response.follow(link, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        data = {
+            'number': re.findall(r'\d+', response.url)[0].lstrip('0'),
+            'name': response.css('h1.page-title::text').get(),
+            'status': response.css(
+                'dt:contains("Status") + dd abbr::text').get(),
+        }
+        yield PepParseItem(data)
