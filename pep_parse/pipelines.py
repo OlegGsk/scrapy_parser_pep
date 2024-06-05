107fc41fe0e07b4380bde3c103e13fb8e39bc827
Олег Говоровский <oleq.gov@ya.ru>
2024-06-05 22:34:38 +0500

    fix pep8

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,13 +1,37 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import csv
+import datetime
+from collections import defaultdict
 
-
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+from pep_parse.constants import BASE_DIR
 
 
 class PepParsePipeline:
+
+    def open_spider(self, spider):
+        self.status_dict = defaultdict(int)
+
+        data_now = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        filename = f'status_summary_{data_now}.csv'
+
+        downloads_dir = BASE_DIR / 'results'
+        downloads_dir.mkdir(exist_ok=True)
+        self.status_summary_path = downloads_dir / filename
+
     def process_item(self, item, spider):
+        self.status_dict[item['status']] += 1
         return item
+
+    def close_spider(self, spider):
+
+        total = sum(self.status_dict.values())
+
+        with open(f'{self.status_summary_path}', 'w', newline=''
+                  ) as csvfile:
+            fieldnames = ['Статус', 'Количество']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+
+            for key, value in self.status_dict.items():
+                writer.writerow({'Статус': key, 'Количество': value})
+
+            writer.writerow({'Статус': 'Total', 'Количество': total})
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,6 +1,8 @@
-import scrapy
-from pep_parse.items import PepParseItem
 import re
+
+import scrapy
+
+from pep_parse.items import PepParseItem
 
 
 class PepSpider(scrapy.Spider):
@@ -11,14 +13,25 @@ class PepSpider(scrapy.Spider):
     def parse(self, response):
         all_links_peps = response.css(
             'section#numerical-index a::attr(href)').getall()
-        for link in all_links_peps:
+        for link in all_links_peps[1:]:
             yield response.follow(link, callback=self.parse_pep)
 
     def parse_pep(self, response):
         data = {
-            'number': re.findall(r'\d+', response.url)[0].lstrip('0'),
-            'name': response.css('h1.page-title::text').get(),
+            'number': self.get_pep_number(response),
+            'name': self.get_pep_name(response),
             'status': response.css(
                 'dt:contains("Status") + dd abbr::text').get(),
         }
         yield PepParseItem(data)
+
+    @classmethod
+    def get_pep_name(cls, response):
+        text = response.css('h1.page-title *::text').getall()
+        name = ' '.join(text)
+        return re.sub(r'PEP \d+\s–\s', '', name,)
+
+    @classmethod
+    def get_pep_number(cls, response):
+        number = response.css('h1.page-title::text').get()
+        return re.findall(r'\d+', number)[0]
